14803473605b78676acf6715f5b8c27ac7c80963
Kuchitama <kiyo.kunihira@gmail.com>
2023-01-24 08:41:40 +0000

    Add wrong sql for test

--- sqls/20230124/wrong.sql
+++ sqls/20230124/wrong.sql
@@ -1,16 +1,16 @@
 UPDATE
-	fanclub_memberships -- comment -- wrong comment
+	fanclub_memberships --comment -- wrong comment
 SET
 	fanclub_memberships.payment_method_id = 1
 WHERE
 	fanclub_memberships.id = 238016;
 
 
-UPDATE fanclub_memberships
+UPDATEfanclub_memberships
 SET	fanclub_memberships.payment_method_id = 1
 WHERE
 	fanclub_memberships.id = 238016
   and fanclub_memberships.id in (1,
 		2,
-		3 -- invalid comma
+		3, -- invalid comma
 	);
