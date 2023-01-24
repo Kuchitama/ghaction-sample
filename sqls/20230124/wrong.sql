dd1dc4493848c5d14bc2b850c67254f6c1502aa6
Kuchitama <kiyo.kunihira@gmail.com>
2023-01-24 17:31:09 +0900

    Fix wrong.sql

--- sqls/20230124/wrong.sql
+++ sqls/20230124/wrong.sql
@@ -1,5 +1,5 @@
 UPDATE
-	fanclub_memberships --comment -- wrong comment
+	fanclub_memberships -- comment -- wrong comment
 SET
 	fanclub_memberships.payment_method_id = 1
 WHERE
@@ -12,5 +12,5 @@ WHERE
 	fanclub_memberships.id = 238016
   and fanclub_memberships.id in (1,
 		2,
-		3, -- invalid comma
+		3 -- invalid comma
 	);
